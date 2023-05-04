8eec83493461ee01573e34970b3a56d807c5f9eb
ABUSAIDSHABIB <92622461+abusaidshabib@users.noreply.github.com>
2023-05-04 18:31:29 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
-import Link from "next/link";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Price from "./components/Price/Price";
-
-
 export default function Home() {
   return (
     <div>
-      {/* <Link href="/check" className="bg-primary-700 py-2 px-5 text-white">Check</Link> */}
-      <Price></Price>
-      <Portfolio></Portfolio>
     </div>
   )
 }
